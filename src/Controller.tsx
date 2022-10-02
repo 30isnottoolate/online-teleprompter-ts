c5ffdd5e7f12ca99c863cc03a3516b76b9f6a08e
30isnottoolate <akos.varga.92@gmail.com>
2022-10-02 18:10:25 +0200

    Implement component methods

--- src/Controller.tsx
+++ src/Controller.tsx
@@ -112,7 +112,13 @@ const Controller: React.FC<ControllerProps> = ({ isActive, setIsActive, mode, se
     return (
         <div
             id="controller"
-            className={isActive ? "transparent" : "visible"}>
+            className={isActive ? "transparent" : "visible"}
+            style={{
+                gridTemplateRows: getGridTemplate(),
+                backgroundColor: theme === "dark" ? "#222222" : "#dddddd",
+                color: theme === "dark" ? "#ffffff" : "#000000",
+                height: getControllerHeight()
+            }}>
             <div id="logo">
                 <h1>
                     Online Teleprompter
@@ -121,59 +127,70 @@ const Controller: React.FC<ControllerProps> = ({ isActive, setIsActive, mode, se
             <div id="main-buttons-group">
                 <button
                     id="start-stop"
-                    className="main-buttons" >
+                    className="main-buttons"
+                    onClick={handleIsActive} >
                     {isActive ? "Stop" : "Start"}
                 </button>
                 <button
                     id="reset"
                     className="main-buttons"
+                    onClick={handleReset}
                     disabled={mode === "edit" ? true : false}>
                     Reset
                 </button>
-                <button id="clear" className="main-buttons" >Clear</button>
+                <button id="clear" className="main-buttons" onClick={handleClear} >Clear</button>
                 <button
                     id="settings-button"
-                    className="main-buttons">
+                    className="main-buttons"
+                    style={{ display: getButtonPresence() }}
+                    onClick={handleIsMenuEnabled}>
                     Settings
                 </button>
             </div>
-            <div id="mode-group" >
+            <div id="mode-group" style={{ display: getDivPresence() }} >
                 <span>Current mode: </span>
                 <button
                     id="mode"
-                    className="mode-buttons" >
+                    className="mode-buttons"
+                    onClick={handleMode} >
                     {mode === "edit" ? "Edit" : "Read"}
                 </button>
                 <span>Color theme: </span>
                 <button
                     id="theme"
-                    className="mode-buttons" >
+                    className="mode-buttons"
+                    onClick={handleTheme} >
                     {theme === "dark" ? "Dark" : "Light"}
                 </button>
             </div>
-            <div id="settings" >
+            <div id="settings" style={{ display: getDivPresence() }} >
                 <label htmlFor="font-size">Font size: </label>
                 <input
                     id="font-size"
                     type="range" min="40" max="150" step="1"
-                    value={fontSize} />
+                    value={fontSize}
+                    onChange={handleFontSize} />
                 <span>{fontSize}</span>
                 <label htmlFor="line-height">Line height: </label>
                 <input
                     id="line-height"
                     type="range" min="1" max="1.5" step="0.01"
-                    value={lineHeight} />
-                <span></span>
+                    value={lineHeight}
+                    onChange={handleLineHeight} />
+                <span>{lineHeight.toFixed(2)}</span>
                 <label htmlFor="text-speed">Text speed: </label>
                 <input
                     id="text-speed"
                     type="range" min="20" max="200" step="1"
-                    value={textSpeed} />
+                    value={textSpeed}
+                    onChange={handleTextSpeed} />
                 <span>{textSpeed}</span>
             </div>
-            <div id="default-container" >
+            <div id="default-container" style={{ display: getDivPresence() }} >
                 <button
-                    id="default">
+                    id="default"
+                    onClick={handleDefault} >
+                    Default
                 </button>
             </div>
         </div>
