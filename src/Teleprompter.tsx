39e49a58b20b43c41468392914a6ced903a52ecf
30isnottoolate <akos.varga.92@gmail.com>
2022-10-02 14:54:35 +0200

    Initialize

---
+++ src/Teleprompter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Teleprompter.css';
+
+const Teleprompter = () => {
+  return (
+    <div id="teleprompter">
+      Hello!
+    </div>
+  );
+}
+
+export default Teleprompter;
