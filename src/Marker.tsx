bf97210032232d448eb67c49aff6fc16dbba24d0
30isnottoolate <akos.varga.92@gmail.com>
2022-11-06 17:44:06 +0100

    Add ref to Marker container

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 import './Teleprompter.css';
 
 interface MarkerProps {
+    textMarkerRef: RefObject<HTMLDivElement>,
     fontSize: number,
     lineHeight: number,
     color: string,
     mode: string,
-    top: number,
+    top: string,
     left: number
 }
 
-const Marker: React.FC<MarkerProps> = ({fontSize, lineHeight, color, mode, top, left}: MarkerProps) => {
+const Marker: React.FC<MarkerProps> = ({ textMarkerRef, fontSize, lineHeight, color, mode, top, left }: MarkerProps) => {
     return (
-        <svg
-            className="text-marker"
-            height={fontSize}
-            fill={color}
-            style={{ display: (mode === "read" ? "initial" : "none"), top: top, left: left, padding: `${fontSize * (lineHeight - 1) / 2} 0` }}
-            viewBox="0 0 57 150">
-            <path d="M 7.00,45.00 C 7.00,45.00 7.00,111.00 7.00,111.00 7.00,111.00 49.00,78.00 49.00,78.00 49.00,78.00 7.00,45.00 7.00,45.00 Z" />
-        </svg>
+        <div ref={textMarkerRef}>
+            <svg
+                className="text-marker"
+                height={fontSize}
+                fill={color}
+                style={{ display: (mode === "read" ? "initial" : "none"), top: top, left: left, padding: `${fontSize * (lineHeight - 1) / 2} 0` }}
+                viewBox="0 0 57 150">
+                <path d="M 7.00,45.00 C 7.00,45.00 7.00,111.00 7.00,111.00 7.00,111.00 49.00,78.00 49.00,78.00 49.00,78.00 7.00,45.00 7.00,45.00 Z" />
+            </svg>
+        </div>
     );
 }
 
