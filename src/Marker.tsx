563a2c202508185ab3ff9b357ab34a936236cc3f
30isnottoolate <akos.varga.92@gmail.com>
2022-11-07 15:57:02 +0100

    Reformat code

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -8,17 +8,23 @@ interface MarkerProps {
     color: string,
     mode: string,
     left: number,
-    top: string 
+    top: string
 }
 
-const Marker: React.FC<MarkerProps> = ({ textMarkerRef, fontSize, lineHeight, color, mode, left, top }: MarkerProps) => {
+const Marker: React.FC<MarkerProps> = 
+({ textMarkerRef, fontSize, lineHeight, color, mode, left, top }: MarkerProps) => {
     return (
         <div ref={textMarkerRef}>
             <svg
                 className="text-marker"
                 height={fontSize}
                 fill={color}
-                style={{ display: (mode === "read" ? "initial" : "none"), left: left, top: top, padding: `${fontSize * (lineHeight - 1) / 2} 0` }}
+                style={{
+                    display: (mode === "read" ? "initial" : "none"),
+                    left: left,
+                    top: top,
+                    padding: `${fontSize * (lineHeight - 1) / 2} 0`
+                }}
                 viewBox="0 0 57 150">
                 <path d="M 7.00,45.00 C 7.00,45.00 7.00,111.00 7.00,111.00 7.00,111.00 49.00,78.00 49.00,78.00 49.00,78.00 7.00,45.00 7.00,45.00 Z" />
             </svg>
