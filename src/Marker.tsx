ba4d7fcef64f8582786ac1ecff7f7015fde85375
30isnottoolate <akos.varga.92@gmail.com>
2022-11-07 15:52:53 +0100

    Reorder code

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -7,18 +7,18 @@ interface MarkerProps {
     lineHeight: number,
     color: string,
     mode: string,
-    top: string,
-    left: number
+    left: number,
+    top: string 
 }
 
-const Marker: React.FC<MarkerProps> = ({ textMarkerRef, fontSize, lineHeight, color, mode, top, left }: MarkerProps) => {
+const Marker: React.FC<MarkerProps> = ({ textMarkerRef, fontSize, lineHeight, color, mode, left, top }: MarkerProps) => {
     return (
         <div ref={textMarkerRef}>
             <svg
                 className="text-marker"
                 height={fontSize}
                 fill={color}
-                style={{ display: (mode === "read" ? "initial" : "none"), top: top, left: left, padding: `${fontSize * (lineHeight - 1) / 2} 0` }}
+                style={{ display: (mode === "read" ? "initial" : "none"), left: left, top: top, padding: `${fontSize * (lineHeight - 1) / 2} 0` }}
                 viewBox="0 0 57 150">
                 <path d="M 7.00,45.00 C 7.00,45.00 7.00,111.00 7.00,111.00 7.00,111.00 49.00,78.00 49.00,78.00 49.00,78.00 7.00,45.00 7.00,45.00 Z" />
             </svg>
