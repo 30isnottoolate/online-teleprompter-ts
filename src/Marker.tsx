d79cb0ffe2d351db9a073867d200dc99f4377977
30isnottoolate <akos.varga.92@gmail.com>
2022-12-14 11:03:52 +0100

    Fix Marker padding for Firefor

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -22,7 +22,7 @@ const Marker: React.FC<MarkerProps> =
                     display: (mode === "read" ? "initial" : "none"),
                     left: left,
                     top: top,
-                    padding: `${fontSize * (lineHeight - 1) / 2} 0`
+                    padding: `${fontSize * (lineHeight - 1) / 2}px 0`
                 }}
                 viewBox="0 0 57 150">
                 <path d="M 7.00,45.00 C 7.00,45.00 7.00,111.00 7.00,111.00 7.00,111.00 49.00,78.00 49.00,78.00 49.00,78.00 7.00,45.00 7.00,45.00 Z" />
