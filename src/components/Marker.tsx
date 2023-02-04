d75ecbce4c5ea6a3f293060ee2aca9eaeb58c0f2
30isnottoolate <akos.varga.92@gmail.com>
2023-02-04 15:25:58 +0100

    Change units to rems, simplify component

--- src/components/Marker.tsx
+++ src/components/Marker.tsx
@@ -3,25 +3,18 @@ import React from 'react';
 interface MarkerProps {
     fontSize: number;
     lineHeight: number;
-    color: string;
-    mode: string;
-    left: number;
-    top: string;
 }
 
 const Marker: React.FC<MarkerProps> = 
-({ fontSize, lineHeight, color, mode, left, top }: MarkerProps) => {
+({ fontSize, lineHeight }: MarkerProps) => {
     return (
         <div>
             <svg
                 className="text-marker"
-                height={fontSize}
-                fill={color}
+                height={fontSize + "rem"}
                 style={{
-                    display: (mode === "read" ? "initial" : "none"),
-                    left: left,
-                    top: top,
-                    padding: `${fontSize * (lineHeight - 1) / 2}px 0`
+                    left: `${fontSize * 0.19}rem`,
+                    padding: `${fontSize * (lineHeight - 1) / 2}rem 0` 
                 }}
                 viewBox="0 0 57 150">
                 <path d="M 7.00,45.00 C 7.00,45.00 7.00,111.00 7.00,111.00 7.00,111.00 49.00,78.00 49.00,78.00 49.00,78.00 7.00,45.00 7.00,45.00 Z" />
