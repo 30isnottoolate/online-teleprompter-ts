7d00908ca864c48e9fdaaedf588d2439497698c2
30isnottoolate <akos.varga.92@gmail.com>
2023-02-06 15:17:45 +0100

    Move contents of Controller comp into parent comp
        This will reduce prop drilling.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect, useRef } from 'react';
 import './App.css';
+import MainButtons from './MainButtons';
+import ModeButtons from './ModeButtons';
+import Settings from './Settings';
 import Slider from "./Slider";
 import Controller from "./Controller";
 
@@ -11,7 +14,7 @@ const DEFAULT_TEXT_SPEED: number = 100;
 const READ_SPEED_COEF: number = 0.0151; // char/ms
 
 const App: React.FC = () => {
-    const remValue = parseInt(window.getComputedStyle(document.body).getPropertyValue("font-size"));
+	const remValue = parseInt(window.getComputedStyle(document.body).getPropertyValue("font-size"));
 
 	const [active, setActive] = useState(false);
 	const [mode, setMode] = useState("edit"); // edit or read
@@ -71,8 +74,8 @@ const App: React.FC = () => {
 	}, [viewportWidth]);
 
 	useEffect(() => {
-        setPosition(7.5 * remValue);
-    }, [fontSize, lineHeight, text]);
+		setPosition(7.5 * remValue);
+	}, [fontSize, lineHeight, text]);
 
 	useEffect(() => {
 		if (theme) {
@@ -130,19 +133,85 @@ const App: React.FC = () => {
 		}
 	}, [position, fontSize, lineHeight]);
 
+	const divPresence = viewportWidth < 44 ?
+		isMenuEnabled ? "grid" : "none"
+		: "grid";
+
+	const changeMode = () => {
+		if (mode === "edit") {
+			setMode("read");
+			setIsMenuEnabled(false);
+		} else {
+			setMode("edit");
+			setActive(false);
+			setPosition(7.5 * remValue);
+		}
+	}
+
+	const defaultSettings = () => {
+		if (viewportWidth < 44) {
+			setFontSize(2.5);
+		} else setFontSize(6.25);
+
+		setLineHeight(1.2);
+		setTextSpeed(100);
+	}
+
+	const gridTemplate = viewportWidth < 44 ?
+		isMenuEnabled ? "repeat(5, auto)" : "repeat(2, auto)"
+		: "auto";
+
+	const controllerHeight = viewportWidth < 44 && isMenuEnabled ? "18.75rem" : "7.5rem";
+
 	return (
 		<>
-			<Controller
-				active={active} setActive={setActive}
-				mode={mode} setMode={setMode}
-				isMenuEnabled={isMenuEnabled} setIsMenuEnabled={setIsMenuEnabled}
-				setPosition={setPosition}
-				viewportWidth={viewportWidth}
-				theme={theme} setTheme={setTheme}
-				setText={setText}
-				fontSize={fontSize} setFontSize={setFontSize}
-				lineHeight={lineHeight} setLineHeight={setLineHeight}
-				textSpeed={textSpeed} setTextSpeed={setTextSpeed} />
+			<div
+				id="controller"
+				className={active ? "transparent" : "visible"}
+				style={{
+					gridTemplateRows: gridTemplate,
+					height: controllerHeight
+				}}>
+				<div id="logo">
+					<h1>
+						Online Teleprompter
+					</h1>
+				</div>
+				<MainButtons
+					active={active}
+					setActive={setActive}
+					mode={mode}
+					setMode={setMode}
+					setIsMenuEnabled={setIsMenuEnabled}
+					setPosition={setPosition}
+					setText={setText}
+				/>
+				<ModeButtons
+					divPresence={divPresence}
+					changeMode={changeMode}
+					mode={mode}
+					theme={theme}
+					setTheme={setTheme}
+				/>
+				<Settings
+					divPresence={divPresence}
+					fontSize={fontSize}
+					setFontSize={setFontSize}
+					lineHeight={lineHeight}
+					setLineHeight={setLineHeight}
+					textSpeed={textSpeed}
+					setTextSpeed={setTextSpeed}
+				/>
+				<div
+					id="default-container"
+					style={{ display: divPresence }} >
+					<button
+						id="default"
+						onClick={defaultSettings} >
+						Default
+					</button>
+				</div>
+			</div>
 			<Slider
 				mode={mode}
 				position={position}
@@ -150,7 +219,8 @@ const App: React.FC = () => {
 				fontSize={fontSize}
 				lineHeight={lineHeight}
 				textContainerRef={textContainerRef}
-				textDisplayRef={textDisplayRef} />
+				textDisplayRef={textDisplayRef}
+			/>
 		</>
 	);
 }
