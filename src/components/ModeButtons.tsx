517a9841374f2d6d070c53b9b7945b060e5fc762
30isnottoolate <akos.varga.92@gmail.com>
2023-02-06 14:57:42 +0100

    Create new component

---
+++ src/components/ModeButtons.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+interface ModeButtonsProps {
+    divPresence: string;
+    changeMode: () => void;
+    mode: string;
+    theme: string;
+    setTheme: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const ModeButtons: React.FC<ModeButtonsProps> =
+    ({ divPresence, changeMode, mode, theme, setTheme }: ModeButtonsProps) => {
+
+        const changeTheme = () => setTheme(prevState => prevState === "light" ? "dark" : "light");
+
+        return (
+            <div
+                id="mode-group"
+                style={{ display: divPresence }} >
+                <span>Current mode: </span>
+                <button
+                    id="mode"
+                    className="mode-buttons"
+                    onClick={changeMode} >
+                    {mode === "edit" ? "Edit" : "Read"}
+                </button>
+                <span>Color theme: </span>
+                <button
+                    id="theme"
+                    className="mode-buttons"
+                    onClick={changeTheme} >
+                    {theme === "dark" ? "Dark" : "Light"}
+                </button>
+            </div>
+        );
+    }
+
+export default ModeButtons;
+////
