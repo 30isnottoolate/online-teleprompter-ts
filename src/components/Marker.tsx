3063a6132324bd871b3608455fa894c3489d99f8
30isnottoolate <akos.varga.92@gmail.com>
2023-02-07 14:25:29 +0100

    Add textMargin state to component

--- src/components/Marker.tsx
+++ src/components/Marker.tsx
@@ -3,17 +3,18 @@ import React from 'react';
 interface MarkerProps {
     fontSize: number;
     lineHeight: number;
+    textMargin: number;
 }
 
 const Marker: React.FC<MarkerProps> = 
-({ fontSize, lineHeight }: MarkerProps) => {
+({ fontSize, lineHeight, textMargin }: MarkerProps) => {
     return (
         <div>
             <svg
                 className="text-marker"
                 height={fontSize + "rem"}
                 style={{
-                    left: `${fontSize * 0.19}rem`,
+                    left: `calc(${fontSize * 0.19}rem + ${textMargin}vw)`,
                     padding: `${fontSize * (lineHeight - 1) / 2}rem 0` 
                 }}
                 viewBox="0 0 57 150">
